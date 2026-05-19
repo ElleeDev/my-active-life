0445a90d53156da0aab73bf26914208c6833ca1d
Ella <leegreat21@gmail.com>
2026-05-20 00:04:47 +0300

    fetch-exercise-detail

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Typography, Stack, Button } from "@mui/material";
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetMuscleImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
 
-const Detail = () => {
-  return <div>Detail</div>;
+const Detail = ({ exerciseDetail }) => {
+  const { id, bodyPart, name, target, equipment } = exerciseDetail;
+  const imageUrl = `https://exercisedb.p.rapidapi.com/image?exerciseId=${id}&resolution=360&rapidapi-key=${process.env.REACT_APP_RAPID_API_KEY}`;
+  return (
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={imageUrl} alt={name} loading="lazy" className="detail-image" />
+    </Stack>
+  );
 };
 
 export default Detail;
